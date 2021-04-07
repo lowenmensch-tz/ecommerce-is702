9377f82ae0d83476d417e90a6de6d7ef2ee5c0d0
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-04-07 09:15:47 -0600

    Actualización de la definición de la base de datos

--- SQL/ecommerce_dds.sql
+++ SQL/ecommerce_dds.sql
@@ -1,13 +1,153 @@
 /*
     @author kenneth.cruz@unah.hn
-    @version 0.1.0
-    @date 14/03/2021
+    @version 0.1.9
+    @date 20/03/2021
 */
 
 
 DROP DATABASE IF EXISTS Ecommerce; 
 CREATE DATABASE Ecommerce CHARACTER SET utf8; 
 USE Ecommerce; 
+
+--
+--  Login
+--  
+
+CREATE TABLE Login(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    tex_email VARCHAR(40) NOT NULL UNIQUE COMMENT "Email con el que entra el sistema"
+    CHECK( tex_email RLIKE "[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(\.[a-zA-Z]+)+"),
+    tex_password TINYTEXT NOT NULL COMMENT "Contraseña"
+) COMMENT "Login";
+
+CREATE TABLE Rol(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_login_fk INT UNSIGNED NOT NULL UNIQUE, 
+    cod_rol ENUM("administrador", "empleado", "cliente") DEFAULT "cliente",
+
+    FOREIGN KEY (id_login_fk) REFERENCES Login(id)
+) COMMENT "Rol de acceso al sistema, dependiendo de este tendrá cierto control de acceso";
+
+
+CREATE TABLE Person(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_login_fk INT UNSIGNED NOT NULL UNIQUE COMMENT "Clave foránea relacionado con el login de usuario",
+    tex_dni TINYTEXT NOT NULL COMMENT "Identificación de usuario asignada por parte de una nación"
+    CHECK( tex_dni RLIKE "[0-9- ]+" ),
+    tex_first_name TINYTEXT NOT NULL COMMENT "Primer nombre de la persona"
+    CHECK( tex_first_name RLIKE "[a-zA-Z ]+" ),
+    tex_last_name TINYTEXT NOT NULL COMMENT "Apellido de la persona"
+    CHECK( tex_first_name RLIKE "[a-zA-Z ]+" ),
+    bit_gender BIT(1) DEFAULT 0 NOT NULL COMMENT "Género: 0 Mujer | 1 Hombre",
+    tim_birthday TIMESTAMP NOT NULL COMMENT "Fecha de nacimiento",
+
+    FOREIGN KEY (id_login_fk) REFERENCES Login(id)
+) COMMENT "Entidad que contiene los atriutos básicos, información necesaria para una persona";
+
+CREATE TABLE Country(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    tex_name VARCHAR(50) NOT NULL UNIQUE COMMENT "Nombre del país",
+    tex_iso TINYTEXT NOT NULL COMMENT "Iso o abreviatura"
+) COMMENT "Nombre de país";
+
+--
+--  Clientes
+--
+
+CREATE TABLE Client(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_person_fk INT UNSIGNED NOT NULL COMMENT "Clave foranea que relaciona esta entidad con la entidad Persona",
+    id_country_fk INT UNSIGNED NOT NULL COMMENT "Clave foranea que relaciona esta entidad con la entidad País",
+    bit_type BIT(1) DEFAULT 0 NOT NULL COMMENT "Cliente poco frecuente: 0 | Cliente por contrato: 1 ",
+
+    FOREIGN KEY (id_person_fk) REFERENCES Person(id)
+) COMMENT "Entidad Cliente";
+
+CREATE TABLE CreditCard(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_client_fk INT UNSIGNED NOT NULL COMMENT "Clave foranea que relaciona esta entidad con la entidad Cliente | Un cliente puede tener varias tarjetas",
+    tex_number VARCHAR(25) NOT NULL UNIQUE COMMENT "Número de la tarjeta de credito"
+     CHECK( tex_number RLIKE "[0-9 ]+" ), 
+    tex_name TINYTEXT NOT NULL COMMENT "Nombre asociado a la tarjeta"
+     CHECK( tex_name RLIKE "([a-zA-Z ]+\.?)+" ),
+    tim_expiration_date TIMESTAMP NOT NULL COMMENT "Fecha de vencimiento",
+    tex_code TINYTEXT NOT NULL COMMENT "Número de la tarjeta de credito"
+     CHECK( tex_number RLIKE "[0-9]{3,5}" ), 
+
+
+    FOREIGN KEY (id_client_fk) REFERENCES Client(id)
+) COMMENT "Entidad Tarjeta de credito | debito";
+
+CREATE TABLE Address(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_client_fk INT UNSIGNED NOT NULL COMMENT "Clave foranea que relaciona esta entidad con la entidad Cliente | Un cliente puede tener varias direcciones",
+    tex_street_address TINYTEXT NOT NULL COMMENT "Dirección de la calle donde reside",
+    tex_number_street TINYTEXT NOT NULL COMMENT "Número de la calle/bloque/zona",
+    tex_zip TINYTEXT NOT NULL COMMENT "Código postal",
+    tex_city TINYTEXT NOT NULL COMMENT "Ciudad",
+    tex_state TINYTEXT NOT NULL COMMENT "Departamento",
+
+    FOREIGN KEY (id_client_fk) REFERENCES Client(id)
+) COMMENT "Dirección donde se harán los envíos de los productos";
+
+--
+--  Empleados
+--
+
+CREATE TABLE Employee(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_person_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Persona",
+    id_mgr INT UNSIGNED COMMENT "ID del gerente del empleado",
+    dec_salary DECIMAL UNSIGNED NOT NULL COMMENT "Sueldo del empleado",
+    tim_date_hire TIMESTAMP DEFAULT NOW() NOT NULL COMMENT "Fecha de contratación del empleado",
+
+    FOREIGN KEY (id_person_fk) REFERENCES Person(id)
+) COMMENT "Entidad Empleado";
+
+CREATE TABLE Store(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    tex_name_store TINYTEXT NOT NULL COMMENT "Nombre de la tienda", 
+    tex_address TINYTEXT NOT NULL COMMENT "Dirección de la tienda" 
+
+) COMMENT "Tienda Física donde laboran los empleados";
+
+CREATE TABLE EmployeeStore(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_employee_fk INT UNSIGNED NOT NULL UNIQUE COMMENT "Clave foránea que relaciona esta entidad con la entidad Empleado",
+    id_store_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Tienda donde laboran fisicamente los empleados",
+    
+    FOREIGN KEY (id_employee_fk) REFERENCES Employee(id),
+    FOREIGN KEY (id_store_fk) REFERENCES Store(id)
+) COMMENT "Relación: Empleado - tienda Fisica donde labora";
+
+CREATE TABLE Cellphone(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_person_fk INT UNSIGNED NOT NULL COMMENT "Clave foranea que relaciona esta entidad con la entidad Proveedor",
+    tex_cellphone_number TINYTEXT NOT NULL COMMENT "Número de télefono"
+     CHECK( tex_cellphone_number RLIKE "([0-9]+((\-)|(\/)|([ ])))+" ),
+
+    FOREIGN KEY (id_person_fk) REFERENCES Person(id)
+) COMMENT "Número de teléfono";
+
+
+--
+--  Proveedor
+--
+
+CREATE TABLE Supplier(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_employee_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Empleado | Empleado que atenderá al proveedor",
+    tex_name_business TINYTEXT NOT NULL COMMENT "Nombre de la empresa que representa",
+    tex_cellphone_business TINYTEXT NOT NULL COMMENT "Número de teléfono de la empresa"
+     CHECK( tex_cellphone_manager RLIKE "([0-9]+((\-)|(\/)|([ ])))+" ),
+    tex_address_business TINYTEXT NOT NULL COMMENT "Dirección de la empresa",
+    tex_name_manager TINYTEXT NOT NULL COMMENT "nombre del encargado o representante de la empresa",
+    tex_cellphone_manager TINYTEXT NOT NULL COMMENT "Número de teléfono del encargado o representante de la empresa"
+     CHECK( tex_cellphone_manager RLIKE "([0-9]+((\-)|(\/)|([ ])))+" ),
+ 
+    FOREIGN KEY (id_employee_fk) REFERENCES Employee(id)
+) COMMENT "Proveedores"; 
+
 
 --
 --  Producto  
@@ -17,7 +157,8 @@ USE Ecommerce;
 CREATE TABLE Manufacturer(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     tex_manufacturer TINYTEXT NOT NULL COMMENT "Fabricante del producto",
-    tex_brand TINYTEXT NOT NULL COMMENT "Marca del producto"
+    tex_brand TINYTEXT NOT NULL COMMENT "Marca del producto", 
+    tex_website TINYTEXT NOT NULL COMMENT "Enlace de la página web"
 ) COMMENT "Entidad que contiene las marcas de los productos";
 
 CREATE TABLE Product(
@@ -28,16 +169,25 @@ CREATE TABLE Product(
     tex_model TINYTEXT NOT NULL COMMENT "Modelo para el producto",
     tex_description TEXT DEFAULT '' COMMENT "Breve descripción del producto que incluya características", 
     tim_year_launch YEAR NOT NULL COMMENT "Fecha de fabricación o año de lanzamiento del producto", 
+    tex_link_photo JSON DEFAULT '{}' NOT NULL COMMENT "Enlace que redirige a un sitio o página que contiene la imagen del producto",
 
     FOREIGN KEY (id_manufacturer_fk) REFERENCES Manufacturer(id)
 ) COMMENT "Entidad que contiene los datos generales de cualquier producto";
 
-CREATE TABLE Operative_system(
+CREATE TABLE OperativeSystem(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    tex_name TINYTEXT NOT NULL COMMENT "Nombre del sistema operativo" 
+    tex_name VARCHAR(20) NOT NULL UNIQUE COMMENT "Nombre del sistema operativo" 
     -- tex_version TINYTEXT DEFAULT '' COMMENT "Version del sistema operativo"
 
 ) COMMENT "Tipo de sistema operativo";
+
+CREATE TABLE VersionOS(
+    id  INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_operative_system_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que apunta a la entidad Sietema operativo",
+    tex_name VARCHAR(20) NOT NULL UNIQUE COMMENT "Nombre de la version" ,
+
+    FOREIGN KEY (id_operative_system_fk) REFERENCES OperativeSystem(id)
+) COMMENT "Versión del Sistema operativo del dispositivo";
 
 CREATE TABLE Port(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
@@ -50,7 +200,7 @@ CREATE TABLE Port(
     FOREIGN KEY (id_product_fk) REFERENCES Product(id)
 ) COMMENT "Numero de puertos";
 
-CREATE TABLE Graphic_card (
+CREATE TABLE GraphicCard (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_product_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
     sma_cores SMALLINT UNSIGNED DEFAULT 0 COMMENT "Números de nucleos de la GPU",
@@ -97,7 +247,7 @@ CREATE TABLE Interface(
 CREATE TABLE Monitor(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_interface_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Interface; hereda sus atributos",
-    dec_response_time DECIMAL(3, 1) UNSIGNED DEFAULT 15 NOT NULL COMMENT "Tiempo de respuesta medido en microsegundos (0.5ms, 6ms, 12ms)", 
+    dec_response_time DECIMAL(3, 1) UNSIGNED DEFAULT 10 NOT NULL COMMENT "Tiempo de respuesta medido en microsegundos (0.5ms, 6ms, 12ms)", 
     sma_refresh_rate SMALLINT DEFAULT 60 NOT NULL COMMENT "Tasa de refresco medido en hertz 60h",
 
     FOREIGN KEY (id_interface_fk) REFERENCES Interface(id)
@@ -107,11 +257,11 @@ CREATE TABLE Monitor(
 CREATE TABLE Tv(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_interface_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
-    id_operative_sistem_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
+    id_version_os_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
     bit_smart BIT(1) DEFAULT 1 COMMENT "Queremos saber sí un tv es smart o no, hacemos la suposición que lo es asignando el valor 1",
 
     FOREIGN KEY (id_interface_fk) REFERENCES Interface(id),
-    FOREIGN KEY (id_operative_sistem_fk) REFERENCES Operative_system(id)
+    FOREIGN KEY (id_version_os_fk) REFERENCES VersionOS(id)
 ) COMMENT "Entidad que contiene la información para los Televisores";
 
 
@@ -124,7 +274,7 @@ CREATE TABLE Audio(
     FOREIGN KEY (id_product_fk) REFERENCES Product(id)
 ) COMMENT "Entidad que contiene las componentes esenciales para las entidades Parlante portatil, Teatro en Casa y Equipo de sonido";
 
-CREATE TABLE Portable_speaker(
+CREATE TABLE PortableSpeaker(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_audio_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
     tim_battery TIME NOT NULL COMMENT "Tiempo de duración de la carga de la bateria medido en horas siguiendo el formato: hh:mm:ss",
@@ -133,7 +283,7 @@ CREATE TABLE Portable_speaker(
     FOREIGN KEY (id_audio_fk) REFERENCES Audio(id)
 ) COMMENT "";
 
-CREATE TABLE Home_theater(
+CREATE TABLE HomeTheater(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_audio_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
     tex_port TINYTEXT DEFAULT "" COMMENT "Puertos disponibles en el tatro en casa",
@@ -141,7 +291,7 @@ CREATE TABLE Home_theater(
     FOREIGN KEY (id_audio_fk) REFERENCES Audio(id)
 ) COMMENT "";
 
-CREATE TABLE Audio_system(
+CREATE TABLE AudioSystem(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_audio_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
     tex_format_reproduction TINYTEXT DEFAULT '' COMMENT "Formatos de reproducción: Cd player, AM-FM...",
@@ -184,13 +334,12 @@ CREATE TABLE Desktop(
 CREATE TABLE Portable(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_computer_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
-    id_operative_sistem_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona el Tipo de sistema operativo con el dispositivo", 
+    id_version_os_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona el Tipo de sistema operativo con el dispositivo", 
     dec_diagonal DECIMAL(3, 1) UNSIGNED NOT NULL COMMENT "Longitud de la diagonal del panel medido en pulgadas",
-    tex_version_os TINYTEXT DEFAULT '' COMMENT "Version del Sistema operativo",
     --    sma_battery SMALLINT NOT NULL COMMENT "Capacidad de la bateria medida en miliamperes", 
 
     FOREIGN KEY (id_computer_fk) REFERENCES Computer(id),
-    FOREIGN KEY (id_operative_sistem_fk) REFERENCES Operative_system(id)
+    FOREIGN KEY (id_version_os_fk) REFERENCES VersionOS(id)
 ) COMMENT "Entidad Portátil mantiene características para las entidades 'Laptop' y 'Celular'";
 
 
@@ -198,7 +347,7 @@ CREATE TABLE Laptop(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     id_portable_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Producto, dado que hereda sus atributos",
     tex_graphic TINYTEXT DEFAULT 'interna' COMMENT "Gráfica dedicada para portátil", 
-    cod_type ENUM("ultrabook", "gaming", "touch", "notebook", "chromebook", "mac") COMMENT "Tipos de laptop de venta en el mercado",
+    cod_type ENUM("notebook", "ultrabook", "gaming", "touch", "chromebook", "mac") COMMENT "Tipos de laptop de venta en el mercado",
     tim_battery TIME DEFAULT '4:00:00' NOT NULL COMMENT "Tiempo de duración de la carga de la bateria medido en horas siguiendo el formato: hh:mm:ss",
 
     FOREIGN KEY (id_portable_fk) REFERENCES Portable(id)
@@ -216,7 +365,6 @@ CREATE TABLE Smartphone(
 
     FOREIGN KEY (id_portable_fk) REFERENCES Portable(id)
 ) COMMENT "Entidad Celular";
-
 
 
 --
@@ -247,7 +395,7 @@ CREATE TABLE Invoice(
 
 ) COMMENT "Factura";
 
-CREATE TABLE Invoice_detail(
+CREATE TABLE InvoiceDetail(
     id INT UNSIGNED AUTO_INCREMENT, 
     id_invoice_fk INT UNSIGNED NOT NULL COMMENT "Clave foranea de la entidad Factura", 
     id_inventory_fk INT UNSIGNED NOT NULL COMMENT "Clave Foranea de la entidad Inventario", 
@@ -261,3 +409,31 @@ CREATE TABLE Invoice_detail(
 ) COMMENT "Detalles de la factura que incluye la información de la cantidad de productos a comprar";
 
 
+--
+--  Pedidos Cliente | Pedidos a Proveedores
+--
+
+CREATE TABLE CustomerOrder (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_client_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Empleado",
+    id_invoice_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Factura",
+    tex_number_order TINYTEXT NOT NULL COMMENT "Número de orden", 
+    tim_delivery_date TIMESTAMP NOT NULL COMMENT "Orden de entrega del producto",
+
+    FOREIGN KEY (id_invoice_fk) REFERENCES Invoice(id),
+    FOREIGN KEY (id_client_fk) REFERENCES Client(id)
+) COMMENT "Pedidos que realiza el cliente";
+
+
+CREATE TABLE SupplierOrder(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    id_supplier_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Proveedor",
+    id_inventory_fk INT UNSIGNED NOT NULL COMMENT "Clave foránea que relaciona esta entidad con la entidad Inventario",
+    
+    tex_number_order TINYTEXT NOT NULL COMMENT "Número de orden",
+    sma_quantity SMALLINT UNSIGNED NOT NULL COMMENT "Cantidad de productos a encargar",
+    tim_order_date TIMESTAMP DEFAULT NOW() COMMENT "Fecha en que se realiza la orden",
+ 
+    FOREIGN KEY (id_inventory_fk) REFERENCES Inventory(id),
+    FOREIGN KEY (id_supplier_fk) REFERENCES Supplier(id)
+) COMMENT "Orden de pedidos que realiza la empresa a los Proveedores cada vez que el inventario se agota"; 
