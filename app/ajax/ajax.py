c4374315468029f65af9fe9847a8bcc4aa6d31e1
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-04-19 18:08:09 -0600

    Se implementa y modifica función que un request de la base de datos al coincidir productos

--- app/ajax/ajax.py
+++ app/ajax/ajax.py
@@ -2,12 +2,39 @@ from app.models import Product
 import traceback
 from flask import Flask , Blueprint, jsonify, request, abort
 import time
+from mariaDB.dbConnection import dbConnectionService
+import json
 
 ajax_bp = Blueprint("ajax_bp", __name__)
 
 @ajax_bp.route("/search")
 def search_query():
+    jso = []
+
+    con = dbConnectionService()
     query = request.args.get("query")
+
+    result = con.findAllProduct(query)
+    
     product = Product()
     product_items = product.show_all_items()
-    return jsonify([dict(p) for p in product_items if p['name'].lower().startswith(query)])
+
+    if product_items: 
+        [
+            jso.append(
+                {
+                    "id":element[0],
+                    "title":element[1],
+                    "photo":element[2],
+                    "price":element[3],
+                    "category":element[4]
+                }
+            )
+            for element in result
+        ]
+    
+    print( jso )
+    print( json.dumps(jso) )
+
+    #return jsonify([dict(p) for p in product_items if p['name'].lower().startswith(query)])
+    return jsonify( jso )
