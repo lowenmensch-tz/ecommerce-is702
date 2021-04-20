f62b1f34b092b271b9ae6f3852cff821c4a2b1ac
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-04-19 18:07:13 -0600

    Se implementa función ajax para la búsqueda de productos en la base de datos

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -1,47 +1,61 @@
 $(document).ready(function(){
- $(".search-results").css("display", "none")
-		$("body").on("click", function(){
-		  	$(".search-results").css("display", "none")
-		})
 
-		if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
-			$('[data-toggle="tooltip"]').tooltip()
-		} 
+	$(".search-results").css("display", "none")
+			$("body").on("click", function(){
+				$(".search-results").css("display", "none")
+			})
 
-		$(".search input").on("keyup", function(e){
-		    value = $(".search input").val();
-		      $(".search-results").css("display", "block")
-		    $.ajax({
-		        url:`/ajax/search?query=${value.toLowerCase()}`
-		    }).done(function(data){
-		      showResults(data)
-		    })
+			if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
+				$('[data-toggle="tooltip"]').tooltip()
+			} 
 
-})
+			$(".search input").on("keyup", function(e){
+				
+				value = $(".search input").val();
 
-function showResults(data){
-  var output="";
-if(data.length === 0 ){
-output = "No Search Results Found"
-$(".search-results .container").html(output)
-}
-else if (data.length <=4 ){
-  data.forEach(item => {
-   output += `
-    <div class="row">
-    <div class="col p-2 ml-3">
-      <a href="/products/view?id=${item.id}">${item.name}</a>
+				$(".search-results").css("display", "block")
+				$.ajax({
+					url:`/ajax/search?query=${value.toLowerCase()}`
+				}).done(function(data){
+					
+					showResults(data)
 
-</div>
-    </div>
-    `
+				})
 
-    $(".search-results").html(output)
-  })
+	})
 
-}
+	function showResults(data){
+		var output = "";
 
+		console.log( data );
 
-}
+		/*
+			data = 
+			[ {"id":element_a, "title":element_b, "photo":element_c, "price":element_d,  "category":element_e}, ... ]
+		*/
+
+		if(data.length === 0 ){
+		
+			output = "No Results Found";
+			$(".search-results .container").html(output);
+
+		}
+
+		else{
+		
+			for (let index = 0; index < data.length; index++) {
+				output += `
+						<div class="row">
+							<div class="col p-2 ml-3">
+								<a href="/products/${data[index]["category"]}/${data[index]["id"]}">${data[index]["title"]}</a>
+							</div>
+						</div>
+						`;
+
+			$(".search-results").html(output);
+
+			}
+		}
+	} // end function
 
 }); 
