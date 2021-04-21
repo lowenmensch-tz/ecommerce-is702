ce9b17e3fb5f3676780aa1a40bb53477d58b9c2c
Lxr-Dev <alexis_danielGT@hotmail.com>
2021-04-21 11:58:16 -0600

    Se completo el formulario de Autenticación y Registro de Usuarios

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, Flask , jsonify, render_template, session, request, redirect, url_for
-from app.models import User
+from flask import Blueprint, Flask , jsonify, render_template, session, request, redirect, url_for, flash
 from .forms import RegistrationForm, LoginForm
 auth_bp = Blueprint("auth_bp", __name__, template_folder="templates/auth")
 
@@ -7,11 +6,11 @@ auth_bp = Blueprint("auth_bp", __name__, template_folder="templates/auth")
 def main():
 	if request.method == "POST":
 		form = LoginForm()
-		user= User()
 		email = request.form['email']
 		password = request.form['password']
-		#result = user.verify(email, password) En esta línea se debe validar que el usuario exista utilizando el objeto de conexión 
-		#if result == True: En caso que el usuario exista se crea la Sesión y se pueden realizar las validaciones en cualquier lugar
+		print(email)
+		print(password)
+		
 		session['email'] = email
 		return redirect(url_for("general_bp.home"))
 	return render_template("login.html", title="Login")
@@ -19,14 +18,53 @@ def main():
 @auth_bp.route("/register", methods=["GET","POST"])
 def signup():
 	if request.method == "POST":
-		form - LoginForm()
-		user= User()
-		fname = request.form['fname']
-		lname = request.form['lname']
-		email = request.form['email']
-		password = request.form['password']
-		user.add(fname,lname, email, password)
-		return redirect(url_for("auth_bp.main"))
+		form = LoginForm()
+		try:
+			fname = request.form['fname'].trim()
+			lname = request.form['lname'].trim()
+			email = request.form['email'].trim()
+			gender = request.form['gender'].trim()
+			country = request.form['fCountry'].trim()
+			city = request.form['fCity'].trim()
+			street = request.form['fcalle'].trim()
+			streetNumber = request.form['fNumeroCalle'].trim()
+			state = request.form['fEstado'].trim()
+			zipCode = request.form['fZip'].trim()
+			idNumber = request.form['idNumber'].trim()
+			creditCardNumber = request.form['creditCard'].trim()
+			CSV = request.form['fCSV'].trim()
+			expireDate = request.form['fCaducidad'].trim()
+			phone = request.form['fPhoneNumber'].trim()
+			birthDate = request.form['fBirthDate'].trim()
+			password = request.form['password'].trim()
+			rPassword = request.form['rPassword'].trim()
+
+			if password == rPassword:
+				return redirect(url_for("auth_bp.main"))
+			else:
+				flash('Las contraseñas no coinciden!')
+				return render_template("signup.html", title ="register")
+
+		except:
+			bussinessName = request.form['fNombreEmpresa'].trim()
+			email = request.form['email'].trim()
+			country = request.form['fCountry'].trim()
+			city = request.form['fCity'].trim()
+			street = request.form['fcalle'].trim()
+			streetNumber = request.form['fNumeroCalle'].trim()
+			state = request.form['fEstado'].trim()
+			zipCode = request.form['fZip'].trim()
+			RTNnumber = request.form['RTNnumber'].trim()
+			accountNumber = request.form['fAccount'].trim()
+			phone = request.form['fTelefono'].trim()
+			password = request.form['password'].trim()
+			rPassword = request.form['rPassword'].trim()
+		
+			if password == rPassword:
+				return redirect(url_for("auth_bp.main"))
+			else:
+				flash('Las contraseñas no coinciden!')
+				return render_template("signup.html", title ="register")
 	else:
 		return render_template("signup.html", title ="register")
 
@@ -34,7 +72,6 @@ def signup():
 def forgot_pass():
 	return render_template("forgot_password.html", title="forgot password")
 
-
 @auth_bp.route("/process_payment", methods=["GET","POST"])
 def process_payment():
-	return render_template("process_payment.html", title="Process payment")
+  return render_template("process_payment.html", title="Process payment")
